b021dbbe9843d92099f890ec95ad0b04e1ae81c4
Olamide Aderogba <G00401971@atu.ie>
2023-10-13 18:51:39 +0100

    Created new class called Books.

---
+++ src/main/java/atu/ie/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
