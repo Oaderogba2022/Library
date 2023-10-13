f19dd38031b8a67da81c32614faee9fe1d94486f
Olamide Aderogba <G00401971@atu.ie>
2023-10-13 19:21:31 +0100

    Created new class called Books and added title, author, ISBN and published year.

--- src/main/java/atu/ie/library/Book.java
+++ src/main/java/atu/ie/library/Book.java
@@ -1,5 +1,16 @@
 package atu.ie.library;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publishedYear;
 
 }
